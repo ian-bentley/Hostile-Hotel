5069a750d40a64de072da3f43dc8b21fb137d3e3
ian-bentley <ibentley981203@gmail.com>
2022-04-08 23:19:59 -0700

    Added player movement and collision

--- Hostile Hotel/Assets/Scripts/PlayerController.cs
+++ Hostile Hotel/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject player;
+    [SerializeField] float speed;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        /*
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(-1 * speed, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(1 * speed, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -1 * speed);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 1 * speed);
+        }
+        */
+
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
     }
 }
